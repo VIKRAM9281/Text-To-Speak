e3509e1b46e26b97d6cb8903ed9afbdef7297cc1
vikram760 <bhosalevikram760.com>
2024-12-26 13:07:59 +0530

    add App.css import and remove welcome message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // src/App.js
 import React from 'react';
 import TextToSpeech from './Components/TextToSpeech';
-
+import './App.css';
 function App() {
 
   return (
     <div>
-      <h1>Welcome to React</h1>
       <TextToSpeech/>
     </div>
   );
